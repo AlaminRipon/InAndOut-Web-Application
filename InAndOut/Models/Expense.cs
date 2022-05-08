c044b7588d29a500d6ae9f9a3ff15b0f48902515
AlaminRipon <alaminripon5654@gmail.com>
2022-05-08 23:14:08 +0600

    Expense Table Create and Retrieve operation performed

---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Item Name")]
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
